46388e016797dd05bbd1eaf0a89e2f5e8126490c
Anand Kumar <kumara0398@gmail.com>
2019-09-01 15:58:17 -0400

    add code
    Signed-off-by: Anand Kumar <kumara0398@gmail.com>

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+# The Fibonacci numbers, form what is called the Fibonacci sequence, such that 
+# each number is the sum of the two preceding ones, starting from 0 and 1.
+
+def fib(n):
+    if n < 2:
+        return n
+    # TODO: fix
+    return fib(n - 1) + fib(n - 1)
+
+def memoized_fib(n):
+    # TODO: implement
+    pass
