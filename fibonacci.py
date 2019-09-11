4fc108be366000fd27139233b04b2daf863729d1
Anand Kumar <kumara0398@gmail.com>
2019-09-11 19:04:17 -0400

    add new function

--- fibonacci.py
+++ fibonacci.py
@@ -4,4 +4,15 @@
 def fib(n):
     if n < 2:
         return n
-    return fib(n - 1) + fib(n - 1)
+    return fib(n - 1) + fib(n - 2)
+
+def fast_fib(n):
+    memo = [0, 1]
+    
+    if n < 2:
+        return memo[n]
+    
+    for x in range(2, n + 1):
+        memo.append(memo[x-2] + memo[x-1])
+    
+    return memo[-1]
